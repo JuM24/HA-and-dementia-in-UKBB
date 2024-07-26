e7b307f2aada87a98beb8bdcd05a6a30414c8d8a
Jure <jure.mur@protonmail.com>
2024-07-26 14:09:52 +0200

    Minor changes.

--- 5_descriptive_stats.R
+++ 5_descriptive_stats.R
@@ -8,44 +8,6 @@ file_name <- 'hearing_masterfile_ITT.rds'
 hear <- readRDS(file_name)
 
 
-# plots of follow-up times
-ggplot(hear, aes(x=follow_up, fill = dementia)) +
-  geom_histogram() +
-  scale_fill_brewer(type = "qual", palette = 6) +
-  scale_x_continuous(expand = c(0, 0), limits = c(0, 15), breaks = seq(0, 20, by = 2)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  xlab("Year") +
-  ylab("Count") +
-  theme_light() +
-  theme(axis.text.x = element_text(face = "plain", size = 12, color = "grey15", angle=0),
-        axis.title.x = element_text(face = "bold", size = 11.5, color = "grey15"),
-        axis.text.y = element_text(face = "plain", size = 12, color = "grey15"),
-        axis.title.y = element_text(face = "bold", size = 12.5, color = "grey15"), legend.position = 'none') 
-
-no_aids <- ggplot(filter(hear, hear_aid_any == 0), aes(x=follow_up)) +
-  geom_histogram(fill = '#e41a1c') +
-  scale_x_continuous(expand = c(0, 0), limits = c(0, 15), breaks = seq(0, 20, by = 2)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  xlab("Year") +
-  ylab("Count") +
-  theme_light() +
-  theme(axis.text.x = element_text(face = "plain", size = 12, color = "grey15", angle=0),
-        axis.title.x = element_text(face = "bold", size = 11.5, color = "grey15"),
-        axis.text.y = element_text(face = "plain", size = 12, color = "grey15"),
-        axis.title.y = element_text(face = "bold", size = 12.5, color = "grey15"), legend.position = 'none') 
-
-aids <- ggplot(filter(hear, hear_aid_any == 1), aes(x=follow_up)) +
-  geom_histogram(fill = '#377eb8') +
-  scale_x_continuous(expand = c(0, 0), limits = c(0, 15), breaks = seq(0, 20, by = 2)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  xlab("Year") +
-  ylab("Count") +
-  theme_light() +
-  theme(axis.text.x = element_text(face = "plain", size = 12, color = "grey15", angle=0),
-        axis.title.x = element_text(face = "bold", size = 11.5, color = "grey15"),
-        axis.text.y = element_text(face = "plain", size = 12, color = "grey15"),
-        axis.title.y = element_text(face = "bold", size = 12.5, color = "grey15"), legend.position = 'none') 
-gridExtra::grid.arrange(no_aids, aids, ncol=2)
 median(hear$follow_up)
 median(hear$follow_up[hear$hear_aid_any == 1])
 median(hear$follow_up[hear$hear_aid_any == 0])
@@ -56,7 +18,8 @@ median(hear$follow_up[hear$hear_aid_any == 0])
 table(hear$hear_aid_any); prop.table(table(hear$hear_aid_any))*100
 table(hear$dementia); prop.table(table(hear$dementia))*100
 
-## outcomes (causes of censoring) for HA and non-HA groups; this is by default for per-protocol; it has to be modified
+## outcomes (causes of censoring) for HA and non-HA groups; 
+# this is by default made for per-protocol; it has to be modified (see below)
 # to display numbers for the intention-to-treat analysis
 ha <- filter(hear, hear_aid_any == 1)
 han <- filter(hear, hear_aid_any == 0)
@@ -142,7 +105,7 @@ round(100*prop.table(table(hear$hl_ehr)),1)
 
 
 
-## different combinations of HL source
+## different combinations of HL sources
 nrow(filter(hear, hl_self_report == 1 & hl_ehr == 0 & hl_srt == 0))
 nrow(filter(hear, hl_self_report == 0 & hl_ehr == 0 & hl_srt == 1))
 nrow(filter(hear, hl_self_report == 0 & hl_ehr == 1 & hl_srt == 0))
@@ -161,10 +124,10 @@ nrow(filter(hear, hl_self_report == 1 & hl_ehr == 1 & hl_srt == 1))
 
 ## hearing aid use
 
-# leave out the ones that received HA after the grace period
-hear_ha <- filter(hear, grace_period == 1 & hear_aid_any == 1)
-table(hear_ha$hear_aid_origin)
-round(100*prop.table(table(hear_ha$hear_aid_origin)),1)
+# subset to just HA users
+hear_ha <- filter(hear, hear_aid_any == 1)
+table(hear_ha$hear_aid_origin, useNA = c('ifany'))
+round(100*prop.table(table(hear_ha$hear_aid_origin, useNA = c('ifany'))),1)
 
 # self-report
 hear_ha$ha_self_report <- 0
@@ -195,54 +158,64 @@ median(filter(hear, hear_aid_any == 1)$age_USE); IQR(filter(hear, hear_aid_any =
 median(filter(hear, hear_aid_any == 0)$age_USE); IQR(filter(hear, hear_aid_any == 0)$age_USE); hist(filter(hear, hear_aid_any == 0)$age_USE)
 median(filter(hear, dementia == 1)$age_USE); IQR(filter(hear, dementia == 1)$age_USE); hist(filter(hear, dementia == 1)$age_USE)
 median(filter(hear, dementia == 0)$age_USE); IQR(filter(hear, dementia == 0)$age_USE); hist(filter(hear, dementia == 0)$age_USE)
+wilcox.test(hear$age_USE[hear$hear_aid_any == 1], hear$age_USE[hear$hear_aid_any == 0])
+
 
 table(hear$sex); prop.table(table(hear$sex))*100
 table(filter(hear, hear_aid_any == 1)$sex); prop.table(table(filter(hear, hear_aid_any == 1)$sex))*100
 table(filter(hear, hear_aid_any == 0)$sex); prop.table(table(filter(hear, hear_aid_any == 0)$sex))*100
 table(filter(hear, dementia == 1)$sex); prop.table(table(filter(hear, dementia == 1)$sex))*100
 table(filter(hear, dementia == 0)$sex); prop.table(table(filter(hear, dementia == 0)$sex))*100
+chisq.test(hear$hear_aid_any, hear$sex)
 
 table(hear$education_USE); prop.table(table(hear$education_USE))*100
 table(filter(hear, hear_aid_any == 1)$education_USE); prop.table(table(filter(hear, hear_aid_any == 1)$education_USE))*100
 table(filter(hear, hear_aid_any == 0)$education_USE); prop.table(table(filter(hear, hear_aid_any == 0)$education_USE))*100
 table(filter(hear, dementia == 1)$education_USE); prop.table(table(filter(hear, dementia == 1)$education_USE))*100
 table(filter(hear, dementia == 0)$education_USE); prop.table(table(filter(hear, dementia == 0)$education_USE))*100
+chisq.test(hear$hear_aid_any, hear$education_USE)
 
 table(hear$tinnitus_sr_USE); prop.table(table(hear$tinnitus_sr_USE))*100
 table(filter(hear, hear_aid_any == 1)$tinnitus_sr_USE); prop.table(table(filter(hear, hear_aid_any == 1)$tinnitus_sr_USE))*100
 table(filter(hear, hear_aid_any == 0)$tinnitus_sr_USE); prop.table(table(filter(hear, hear_aid_any == 0)$tinnitus_sr_USE))*100
 table(filter(hear, dementia == 1)$tinnitus_sr_USE); prop.table(table(filter(hear, dementia == 1)$tinnitus_sr_USE))*100
 table(filter(hear, dementia == 0)$tinnitus_sr_USE); prop.table(table(filter(hear, dementia == 0)$tinnitus_sr_USE))*100
+chisq.test(hear$hear_aid_any, hear$tinnitus_sr_USE)
 
 table(hear$head_inj); prop.table(table(hear$head_inj))*100
 table(filter(hear, hear_aid_any == 1)$head_inj); prop.table(table(filter(hear, hear_aid_any == 1)$head_inj))*100
 table(filter(hear, hear_aid_any == 0)$head_inj); prop.table(table(filter(hear, hear_aid_any == 0)$head_inj))*100
 table(filter(hear, dementia == 1)$head_inj); prop.table(table(filter(hear, dementia == 1)$head_inj))*100
 table(filter(hear, dementia == 0)$head_inj); prop.table(table(filter(hear, dementia == 0)$head_inj))*100
+chisq.test(hear$hear_aid_any, hear$head_inj)
 
 table(hear$ethnicity_simple); prop.table(table(hear$ethnicity_simple))*100
 table(filter(hear, hear_aid_any == 1)$ethnicity_simple); prop.table(table(filter(hear, hear_aid_any == 1)$ethnicity_simple))*100
 table(filter(hear, hear_aid_any == 0)$ethnicity_simple); prop.table(table(filter(hear, hear_aid_any == 0)$ethnicity_simple))*100
 table(filter(hear, dementia == 1)$ethnicity_simple); prop.table(table(filter(hear, dementia == 1)$ethnicity_simple))*100
 table(filter(hear, dementia == 0)$ethnicity_simple); prop.table(table(filter(hear, dementia == 0)$ethnicity_simple))*100
+chisq.test(hear$hear_aid_any, hear$ethnicity_simple)
 
 median(hear$deprivation); IQR(hear$deprivation); hist(hear$deprivation)
 median(filter(hear, hear_aid_any == 1)$deprivation); IQR(filter(hear, hear_aid_any == 1)$deprivation); hist(filter(hear, hear_aid_any == 1)$deprivation)
 median(filter(hear, hear_aid_any == 0)$deprivation); IQR(filter(hear, hear_aid_any == 0)$deprivation); hist(filter(hear, hear_aid_any == 0)$deprivation)
 median(filter(hear, dementia == 1)$deprivation); IQR(filter(hear, dementia == 1)$deprivation); hist(filter(hear, dementia == 1)$deprivation)
 median(filter(hear, dementia == 0)$deprivation); IQR(filter(hear, dementia == 0)$deprivation); hist(filter(hear, dementia == 0)$deprivation)
+wilcox.test(hear$deprivation[hear$hear_aid_any == 1], hear$age_USE[hear$hear_aid_any == 0])
 
 median(hear$g_USE); IQR(hear$g_USE); hist(hear$g_USE)
 median(filter(hear, hear_aid_any == 1)$g_USE); IQR(filter(hear, hear_aid_any == 1)$g_USE); hist(filter(hear, hear_aid_any == 1)$g_USE)
 median(filter(hear, hear_aid_any == 0)$g_USE); IQR(filter(hear, hear_aid_any == 0)$g_USE); hist(filter(hear, hear_aid_any == 0)$g_USE)
 median(filter(hear, dementia == 1)$g_USE); IQR(filter(hear, dementia == 1)$g_USE); hist(filter(hear, dementia == 1)$g_USE)
 median(filter(hear, dementia == 0)$g_USE); IQR(filter(hear, dementia == 0)$g_USE); hist(filter(hear, dementia == 0)$g_USE)
+t.test(hear$g_USE[hear$hear_aid_any == 1], hear$age_USE[hear$hear_aid_any == 0])
 
 median(hear$srt_min_USE); IQR(hear$srt_min_USE); hist(hear$srt_min_USE)
 median(filter(hear, hear_aid_any == 1)$srt_min_USE); IQR(filter(hear, hear_aid_any == 1)$srt_min_USE); hist(filter(hear, hear_aid_any == 1)$srt_min_USE)
 median(filter(hear, hear_aid_any == 0)$srt_min_USE); IQR(filter(hear, hear_aid_any == 0)$srt_min_USE); hist(filter(hear, hear_aid_any == 0)$srt_min_USE)
 median(filter(hear, dementia == 1)$srt_min_USE); IQR(filter(hear, dementia == 1)$srt_min_USE); hist(filter(hear, dementia == 1)$srt_min_USE)
 median(filter(hear, dementia == 0)$srt_min_USE); IQR(filter(hear, dementia == 0)$srt_min_USE); hist(filter(hear, dementia == 0)$srt_min_USE)
+wilcox.test(hear$srt_min_USE[hear$hear_aid_any == 1], hear$age_USE[hear$hear_aid_any == 0])
 
 mean(hear$srt_min_USE); sd(hear$srt_min_USE); hist(hear$srt_min_USE)
 mean(filter(hear, hear_aid_any == 1)$srt_min_USE); sd(filter(hear, hear_aid_any == 1)$srt_min_USE); hist(filter(hear, hear_aid_any == 1)$srt_min_USE)
@@ -255,6 +228,7 @@ table(filter(hear, hear_aid_any == 1)$data_provider_freq); prop.table(table(filt
 table(filter(hear, hear_aid_any == 0)$data_provider_freq); prop.table(table(filter(hear, hear_aid_any == 0)$data_provider_freq))*100
 table(filter(hear, dementia == 1)$data_provider_freq); prop.table(table(filter(hear, dementia == 1)$data_provider_freq))*100
 table(filter(hear, dementia == 0)$data_provider_freq); prop.table(table(filter(hear, dementia == 0)$data_provider_freq))*100
+chisq.test(hear$hear_aid_any, hear$data_provider_freq)
 
 median(hear$follow_up); IQR(hear$follow_up); hist(hear$follow_up)
 median(filter(hear, hear_aid_any == 1)$follow_up); IQR(filter(hear, hear_aid_any == 1)$follow_up); hist(filter(hear, hear_aid_any == 1)$follow_up)
@@ -267,21 +241,25 @@ table(filter(hear, hear_aid_any == 1)$soc_isol_USE); prop.table(table(filter(hea
 table(filter(hear, hear_aid_any == 0)$soc_isol_USE); prop.table(table(filter(hear, hear_aid_any == 0)$soc_isol_USE))*100
 table(filter(hear, dementia == 1)$soc_isol_USE); prop.table(table(filter(hear, dementia == 1)$soc_isol_USE))*100
 table(filter(hear, dementia == 0)$soc_isol_USE); prop.table(table(filter(hear, dementia == 0)$soc_isol_USE))*100
+chisq.test(hear$hear_aid_any, hear$soc_isol_USE)
 
 table(hear$mood_dis); prop.table(table(hear$mood_dis))*100
 table(filter(hear, hear_aid_any == 1)$mood_dis); prop.table(table(filter(hear, hear_aid_any == 1)$mood_dis))*100
 table(filter(hear, hear_aid_any == 0)$mood_dis); prop.table(table(filter(hear, hear_aid_any == 0)$mood_dis))*100
 table(filter(hear, dementia == 1)$mood_dis); prop.table(table(filter(hear, dementia == 1)$mood_dis))*100
 table(filter(hear, dementia == 0)$mood_dis); prop.table(table(filter(hear, dementia == 0)$mood_dis))*100
+chisq.test(hear$hear_aid_any, hear$mood_dis)
 
 table(hear$inpatient_contact_cat); prop.table(table(hear$inpatient_contact_cat))*100
 table(filter(hear, hear_aid_any == 1)$inpatient_contact_cat); prop.table(table(filter(hear, hear_aid_any == 1)$inpatient_contact_cat))*100
 table(filter(hear, hear_aid_any == 0)$inpatient_contact_cat); prop.table(table(filter(hear, hear_aid_any == 0)$inpatient_contact_cat))*100
 table(filter(hear, dementia == 1)$inpatient_contact_cat); prop.table(table(filter(hear, dementia == 1)$inpatient_contact_cat))*100
 table(filter(hear, dementia == 0)$inpatient_contact_cat); prop.table(table(filter(hear, dementia == 0)$inpatient_contact_cat))*100
+chisq.test(hear$hear_aid_any, hear$inpatient_contact_cat)
 
 table(hear$gp_contact_cat); prop.table(table(hear$gp_contact_cat))*100
 table(filter(hear, hear_aid_any == 1)$gp_contact_cat); prop.table(table(filter(hear, hear_aid_any == 1)$gp_contact_cat))*100
 table(filter(hear, hear_aid_any == 0)$gp_contact_cat); prop.table(table(filter(hear, hear_aid_any == 0)$gp_contact_cat))*100
 table(filter(hear, dementia == 1)$gp_contact_cat); prop.table(table(filter(hear, dementia == 1)$gp_contact_cat))*100
 table(filter(hear, dementia == 0)$gp_contact_cat); prop.table(table(filter(hear, dementia == 0)$gp_contact_cat))*100
+chisq.test(hear$hear_aid_any, hear$gp_contact_cat)
