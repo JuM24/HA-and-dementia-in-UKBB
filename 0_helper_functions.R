76a7b58b07cc5d54317debbdeeafdd274d7bfeab
Jure <jure.mur@protonmail.com>
2024-07-29 08:50:28 +0200

    Minor changes.

--- 0_helper_functions.R
+++ 0_helper_functions.R
@@ -132,6 +132,27 @@ education_classify <- function(x){
 }
 
 
+
+# classification of physical activity frequency into three levels
+# 0 - (none, low [light household tasks only], 
+# 1 - medium [heavy household tasks and/or walking for pleasure and/or other exercise], 
+# 2 - high [strenuous sports]).
+phys_act_classify <- function(x){
+  if (any(x == 3 & !is.na(x), na.rm = TRUE)){
+    return(3)
+  } else if (any((x == 1 | x == 2 | x == 5) & !is.na(x), na.rm = TRUE)){
+    return(2)
+  } else if(any(x == 4 & !is.na(x), na.rm = TRUE)){
+    return(1)
+  } else if (any (x == -7  & !is.na(x), na.rm = TRUE)){
+    return(0)
+  } else {
+    return(NA)
+  }
+}
+
+
+
 # survival plots for imputed data
 analyse_imputed_data <- function(dataset) {
   dataset$follow_up <- as.numeric(dataset$follow_up)
